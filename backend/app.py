d12f1d088b00bb0f888eab46117985439dc7e09b
Punya Kumar <your_email@gmail.com>
2026-05-08 14:13:21 +0530

    Fixed Render deployment

--- backend/app.py
+++ backend/app.py
@@ -570,8 +570,13 @@ def report():
 
 
 
-
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, use_reloader=False, host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
 
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
