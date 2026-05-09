f5e71b075f6ecf9ccae6d6ed10a823fab5e9ae08
alywnalan <alywnalan@gmail.com>
2026-05-09 22:49:17 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from backend.app import app, init_db
+import os
+
+
+if os.environ.get("FINOVA_DB"):
+    init_db()
 
 
 if __name__ == '__main__':
-    import os
-
     init_db()
-    app.run(debug=True, use_reloader=False, host='127.0.0.1', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
